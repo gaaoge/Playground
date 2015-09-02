35f57eafb10c4d0de44fce3ae25aae844dfe8dca
GG <723687948@qq.com>
2015-09-02 20:23:44 +0800

    WebSocket下扫描二维码通信的小demo。

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -8,20 +8,26 @@ var wss = new WebSocketServer({
     port: 3001
 });
 
-wss.broadcast = function (data) {
-    for (var i in this.clients) {
-        this.clients[i].send(JSON.stringify(data));
-    }
-};
-
-wss.on('connection', function(ws) {
+wss.on('connection', function (ws) {
     console.log('connection');
 
-    ws.on('message', function(data) {
-        console.log(data);
-        wss.broadcast(data);
+    ws.on('message', function (data) {
+        data = JSON.parse(data);
+        switch (data.type) {
+            case 'target':
+                ws.id = data.target;
+                break;
+            case 'msg':
+                for (var i in wss.clients) {
+                    if (wss.clients[i].id == data.target) {
+                        wss.clients[i].send(JSON.stringify({message: data.message}));
+                    }
+                }
+                break;
+        }
     });
-    ws.on('close', function() {
+
+    ws.on('close', function () {
         console.log('close');
     });
 });
