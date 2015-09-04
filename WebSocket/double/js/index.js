9f9916b2b16fce6c9af93b23b3a9acba542709ac
GG <723687948@qq.com>
2015-09-04 15:58:47 +0800

    添加扔二维码小demo。

---
+++ WebSocket/double/js/index.js
@@ -0,0 +1,69 @@
+/**
+ * Created by GG on 15/9/4.
+ */
+
+(function ($) {
+    "use strict";
+
+    $(function () {
+        var $qrcode = $('#qrcode');
+        var ws = new WebSocket("ws://192.168.10.105:3002");
+
+        ws.onopen = function (e) {
+            console.log('open');
+        };
+
+        ws.onclose = function (e) {
+            console.log('close');
+        };
+
+        ws.onerror = function (e) {
+            console.error('error');
+        };
+
+        ws.onmessage = function (e) {
+            console.log('message', e.data);
+            var data = JSON.parse(e.data);
+            switch (data.type) {
+                case 'register':
+                    var id = getSearch('id');
+                    if (!id) {
+                        new QRCode('qrcode').makeCode(getAbsPath('') + '?id=' + data.id);
+                    }else {
+                        new QRCode('qrcode').makeCode(getAbsPath('') + '?id=' + id);
+                        ws.send(JSON.stringify({type: 'target', target: id}));
+                        $qrcode.hide();
+                    }
+
+                    $qrcode.on('swipeRight', function () {
+                        $qrcode.removeClass().addClass('slideOutRight');
+                        ws.send(JSON.stringify({type: 'msg', message:'swipeRight'}));
+                    }).on('swipeLeft', function () {
+                        $qrcode.removeClass().addClass('slideOutLeft');
+                        ws.send(JSON.stringify({type: 'msg', message:'swipeLeft'}));
+                    });
+                    break;
+                case 'msg':
+                    $qrcode.show();
+                    if(data.message == 'swipeLeft') {
+                        $qrcode.removeClass().addClass('slideInRight');
+                    }else if(data.message == 'swipeRight') {
+                        $qrcode.removeClass().addClass('slideInLeft');
+                    }
+                    break;
+            }
+        };
+
+        function getSearch(name) {
+            var data = {}, reg = new RegExp("([^?=&]+)(=([^&]*))?", "g"), search = location.search;
+            search && search.replace(reg, function ($0, $1, $2, $3) {
+                data[$1] = $3;
+            });
+            return data[name];
+        }
+
+        function getAbsPath(url) {
+            return location.origin + location.pathname + url;
+        }
+    });
+}(Zepto));
---
+++ WebSocket/double/server.js
@@ -0,0 +1,45 @@
+/**
+ * Created by GG on 15/9/2.
+ */
+
+var WebSocketServer = require('ws').Server;
+
+var wsServer = new WebSocketServer({
+    port: 3002
+});
+
+wsServer.on('connection', function (ws) {
+    console.log('connection');
+
+    ws.on('message', function (data) {
+        data = JSON.parse(data);
+        switch (data.type) {
+            case 'target':
+                ws.target = [data.target];
+                for (var i in wsServer.clients) {
+                    if (wsServer.clients[i].id == data.target) {
+                        wsServer.clients[i].target = wsServer.clients[i].target || [];
+                        wsServer.clients[i].target.push(ws.id);
+                    }
+                    console.log('target', wsServer.clients[i].target);
+                }
+                break;
+            case 'msg':
+                for (var i in wsServer.clients) {
+                    if (ws.target && ws.target.indexOf(wsServer.clients[i].id) != -1) {
+                        wsServer.clients[i].send(JSON.stringify(data));
+                    }
+                }
+                break;
+        }
+    });
+
+    ws.on('close', function () {
+        console.log('close');
+    });
+
+    ws.id = Date.now().toString();
+    ws.send(JSON.stringify({type: 'register', id: ws.id}));
+});
+
+
